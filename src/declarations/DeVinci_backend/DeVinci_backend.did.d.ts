6cf2f2b17d892ccb4ede783df86038d6ccf5c03b
Netrobe Ubuntu <netrobepy@gmail.com>
2024-09-15 12:48:06 +0100

    Fixing deps

--- src/declarations/DeVinci_backend/DeVinci_backend.did.d.ts
+++ src/declarations/DeVinci_backend/DeVinci_backend.did.d.ts
@@ -3,64 +3,69 @@ import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
 export type ApiError = { 'ZeroAddress' : null } |
+  { 'NotFound' : string } |
   { 'InvalidTokenId' : null } |
   { 'Unauthorized' : null } |
   { 'Other' : string };
-export interface Chat {
-  'id' : string,
-  'messages' : Array<Message>,
-  'owner' : Principal,
-  'creationTime' : bigint,
-  'chatTitle' : string,
-  'firstMessagePreview' : string,
+export interface Backend {
+  'addAcls' : ActorMethod<[Principal], undefined>,
+  'addQuestion' : ActorMethod<[string, Question], Result>,
+  'changeApiKey' : ActorMethod<[string], Result_2>,
+  'changeOwner' : ActorMethod<[string], Result_2>,
+  'check_course_has_memory_vectors_entry' : ActorMethod<[string], Result_1>,
+  'createCourse' : ActorMethod<[string, string], Result>,
+  'createResource' : ActorMethod<
+    [string, string, string, ResourceType__1],
+    Result
+  >,
+  'delete_email_subscriber' : ActorMethod<[string], boolean>,
+  'enrollCourse' : ActorMethod<[string, string], Result>,
+  'generateQuestionsForCourse' : ActorMethod<[string], Result_3>,
+  'generateRandomNumber' : ActorMethod<[bigint], bigint>,
+  'getAcls' : ActorMethod<[], Array<Principal>>,
+  'getCourseQuestions' : ActorMethod<[string], Result_7>,
+  'getCourseResources' : ActorMethod<[string], Result_10>,
+  'getOwner' : ActorMethod<[], Principal>,
+  'getProfile' : ActorMethod<[string], Result_9>,
+  'getRandomCourseQuestions' : ActorMethod<[string, bigint], Result_7>,
+  'getRunMessage' : ActorMethod<[string, string], Result_8>,
+  'getRunQuestions' : ActorMethod<[string], Result_7>,
+  'getRunStatus' : ActorMethod<[string], Result_6>,
+  'getRunsInQueue' : ActorMethod<[], Array<SharedThreadRun>>,
+  'getUserEnrolledCourse' : ActorMethod<[string, string], Result_5>,
+  'get_course_memory_vectors' : ActorMethod<[string], Result_4>,
+  'get_education_experiences' : ActorMethod<[], Array<EducationExperience>>,
+  'get_email_subscribers' : ActorMethod<[], Array<[string, EmailSubscriber]>>,
+  'get_icrc1_token_canister_id' : ActorMethod<[], string>,
+  'listCourses' : ActorMethod<[], Array<SharedCourse>>,
+  'registerUser' : ActorMethod<[string], Result>,
+  'removeAcls' : ActorMethod<[Principal], Result_2>,
+  'removeQuestion' : ActorMethod<[string, string], Result_2>,
+  'removeResource' : ActorMethod<[string, string], Result_2>,
+  'sendMessage' : ActorMethod<[string, string, string], Result_3>,
+  'setAssistantId' : ActorMethod<[string], Result_2>,
+  'setRunProcess' : ActorMethod<[string, boolean], Result_2>,
+  'set_icrc1_token_canister' : ActorMethod<[string], Result_2>,
+  'store_course_memory_vectors' : ActorMethod<
+    [string, Array<MemoryVector>],
+    Result_1
+  >,
+  'submitQuestionsAttempt' : ActorMethod<
+    [string, Array<SubmittedAnswer>, string],
+    Result
+  >,
+  'submit_signup_form' : ActorMethod<[SignUpFormInput], string>,
+  'transform' : ActorMethod<[TransformArgs], CanisterHttpResponsePayload>,
+  'updateCourse' : ActorMethod<[string, string, string], Result>,
 }
-export type ChatCreationResult = { 'Ok' : string } |
-  { 'Err' : ApiError };
-export type ChatIdResult = { 'Ok' : string } |
-  { 'Err' : ApiError };
-export interface ChatPreview {
-  'id' : string,
-  'creationTime' : bigint,
-  'chatTitle' : string,
-  'firstMessagePreview' : string,
+export interface CanisterHttpResponsePayload {
+  'status' : bigint,
+  'body' : Uint8Array | number[],
+  'headers' : Array<HttpHeader>,
 }
-export type ChatResult = { 'Ok' : Chat } |
-  { 'Err' : ApiError };
-export type ChatsPreviewResult = { 'Ok' : Array<ChatPreview> } |
-  { 'Err' : ApiError };
-export type ChatsResult = { 'Ok' : Array<Chat> } |
-  { 'Err' : ApiError };
 export type DatabaseToInclude = { 'Local' : null } |
   { 'None' : null } |
   { 'External' : null };
-export interface DeVinciBackend {
-  'check_caller_has_memory_vectors_entry' : ActorMethod<
-    [],
-    MemoryVectorsCheckResult
-  >,
-  'create_chat' : ActorMethod<[Array<Message>], ChatCreationResult>,
-  'delete_chat' : ActorMethod<[string], ChatResult>,
-  'delete_email_subscriber' : ActorMethod<[string], boolean>,
-  'get_caller_chat_history' : ActorMethod<[], ChatsPreviewResult>,
-  'get_caller_chats' : ActorMethod<[], ChatsResult>,
-  'get_caller_memory_vectors' : ActorMethod<[], MemoryVectorsResult>,
-  'get_caller_settings' : ActorMethod<[], UserSettingsResult>,
-  'get_chat' : ActorMethod<[string], ChatResult>,
-  'get_education_experiences' : ActorMethod<[], Array<EducationExperience>>,
-  'get_email_subscribers' : ActorMethod<[], Array<[string, EmailSubscriber]>>,
-  'greet' : ActorMethod<[string], string>,
-  'store_user_chats_memory_vectors' : ActorMethod<
-    [Array<MemoryVector>],
-    MemoryVectorsStoredResult
-  >,
-  'submit_signup_form' : ActorMethod<[SignUpFormInput], string>,
-  'update_caller_settings' : ActorMethod<
-    [UserSettings],
-    UpdateUserSettingsResult
-  >,
-  'update_chat_messages' : ActorMethod<[string, Array<Message>], ChatIdResult>,
-  'update_chat_metadata' : ActorMethod<[UpdateChatObject], ChatIdResult>,
-}
 export interface EducationExperience {
   'id' : string,
   'isStandaloneApp' : boolean,
@@ -83,29 +88,124 @@ export interface EmailSubscriber {
 export type ExperienceType = { 'Onchain' : null } |
   { 'Offchain' : null } |
   { 'Ondevice' : null };
+export interface HttpHeader { 'value' : string, 'name' : string }
+export interface HttpResponsePayload {
+  'status' : bigint,
+  'body' : Uint8Array | number[],
+  'headers' : Array<HttpHeader>,
+}
 export interface MemoryVector {
   'content' : string,
   'metadata' : MemoryVectorMetadata,
   'embedding' : Array<number>,
 }
 export interface MemoryVectorMetadata { 'id' : bigint }
-export type MemoryVectorsCheckResult = { 'Ok' : boolean } |
-  { 'Err' : ApiError };
-export type MemoryVectorsResult = { 'Ok' : Array<MemoryVector> } |
-  { 'Err' : ApiError };
-export type MemoryVectorsStoredResult = { 'Ok' : boolean } |
-  { 'Err' : ApiError };
-export interface Message { 'content' : string, 'sender' : string }
+export interface Message {
+  'content' : string,
+  'role' : MessgeType,
+  'runId' : [] | [string],
+}
+export interface Message__1 {
+  'content' : string,
+  'role' : MessgeType,
+  'runId' : [] | [string],
+}
+export type MessgeType = { 'System' : null } |
+  { 'User' : null };
+export interface Question {
+  'id' : string,
+  'correctOption' : bigint,
+  'description' : string,
+  'options' : Array<QuestionOption>,
+}
+export interface QuestionOption { 'option' : bigint, 'description' : string }
+export interface Resource {
+  'id' : string,
+  'url' : string,
+  'title' : string,
+  'rType' : ResourceType,
+}
+export type ResourceType = { 'Book' : null } |
+  { 'Article' : null } |
+  { 'Slides' : null } |
+  { 'Video' : null };
+export type ResourceType__1 = { 'Book' : null } |
+  { 'Article' : null } |
+  { 'Slides' : null } |
+  { 'Video' : null };
+export type Result = { 'ok' : string } |
+  { 'err' : ApiError };
+export type Result_1 = { 'ok' : boolean } |
+  { 'err' : ApiError };
+export type Result_10 = { 'ok' : Array<Resource> } |
+  { 'err' : ApiError };
+export type Result_2 = { 'ok' : null } |
+  { 'err' : ApiError };
+export type Result_3 = { 'ok' : SendMessageStatus } |
+  { 'err' : SendMessageStatus };
+export type Result_4 = { 'ok' : Array<MemoryVector> } |
+  { 'err' : ApiError };
+export type Result_5 = { 'ok' : SharedEnrolledCourse } |
+  { 'err' : ApiError };
+export type Result_6 = { 'ok' : RunStatus__1 } |
+  { 'err' : ApiError };
+export type Result_7 = { 'ok' : Array<Question> } |
+  { 'err' : ApiError };
+export type Result_8 = { 'ok' : Message__1 } |
+  { 'err' : ApiError };
+export type Result_9 = { 'ok' : SharedUser } |
+  { 'err' : ApiError };
+export type RunStatus = { 'Failed' : null } |
+  { 'Cancelled' : null } |
+  { 'InProgress' : null } |
+  { 'Completed' : null } |
+  { 'Expired' : null };
+export type RunStatus__1 = { 'Failed' : null } |
+  { 'Cancelled' : null } |
+  { 'InProgress' : null } |
+  { 'Completed' : null } |
+  { 'Expired' : null };
+export type SendMessageStatus = { 'Failed' : string } |
+  { 'ThreadLock' : { 'runId' : string } } |
+  { 'Completed' : { 'runId' : string } };
+export interface SharedCourse {
+  'id' : string,
+  'title' : string,
+  'summary' : string,
+}
+export interface SharedEnrolledCourse {
+  'id' : string,
+  'messages' : Array<Message>,
+  'completed' : boolean,
+  'threadId' : string,
+}
+export interface SharedThreadRun {
+  'job' : ThreadRunJob,
+  'status' : RunStatus,
+  'lastExecuted' : [] | [Time],
+  'timestamp' : Time,
+  'threadId' : string,
+  'processing' : boolean,
+  'runId' : string,
+}
+export interface SharedUser {
+  'id' : string,
+  'principal' : [] | [Principal],
+  'fullname' : string,
+  'claimableTokens' : bigint,
+}
 export interface SignUpFormInput {
   'emailAddress' : string,
   'pageSubmittedFrom' : string,
 }
-export interface UpdateChatObject { 'id' : string, 'chatTitle' : string }
-export type UpdateUserSettingsResult = { 'Ok' : boolean } |
-  { 'Err' : ApiError };
-export interface UserSettings { 'selectedAiModelId' : string }
-export type UserSettingsResult = { 'Ok' : UserSettings } |
-  { 'Err' : ApiError };
-export interface _SERVICE extends DeVinciBackend {}
+export interface SubmittedAnswer { 'option' : bigint, 'questionId' : string }
+export type ThreadRunJob = { 'Question' : null } |
+  { 'Message' : null };
+export type Time = bigint;
+export interface TransformArgs {
+  'context' : Uint8Array | number[],
+  'response' : HttpResponsePayload,
+}
+export interface _SERVICE extends Backend {}
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
---
+++ src/tests/actor.js
@@ -0,0 +1,38 @@
+import { Actor, HttpAgent } from "@dfinity/agent";
+import canisterIds from "../../.dfx/local/canister_ids.json";
+import fetch from "isomorphic-fetch";
+import { idlFactory } from "../declarations/DeVinci_backend/DeVinci_backend.did";
+
+export const CANISTER_ID = canisterIds.DeVinci_backend.local;
+console.log("CANISTER_ID", CANISTER_ID);
+
+export const createActor = (canisterId, options = {}) => {
+  const agent = options.agent || HttpAgent.createSync({ ...options.agentOptions });
+
+  if (options.agent && options.agentOptions) {
+    console.warn(
+      "Detected both agent and agentOptions passed to createActor. Ignoring agentOptions and proceeding with the provided agent."
+    );
+  }
+
+  // Fetch root key for certificate validation during development
+  if (process.env.DFX_NETWORK !== "ic") {
+    agent.fetchRootKey().catch((err) => {
+      console.warn(
+        "Unable to fetch root key. Check to ensure that your local replica is running"
+      );
+      console.error(err);
+    });
+  }
+
+  // Creates an actor with using the candid interface and the HttpAgent
+  return Actor.createActor(idlFactory, {
+    agent,
+    canisterId,
+    ...options.actorOptions,
+  });
+};
+
+export const actor = createActor(CANISTER_ID, {
+  agentOptions: { host: "http://127.0.0.1:4943", fetch },
+});
